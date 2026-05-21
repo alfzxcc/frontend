40ed3fbd966a20a4efcd0f60266834205fa20024
Alfie <alfie@example.com>
2026-05-22 01:25:01 +0800

    jwt.interceptor.ts

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -8,14 +8,11 @@ import { AccountService } from '@app/_services';
 export class JwtInterceptor implements HttpInterceptor {
     constructor(private accountService: AccountService) { }
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // add auth header with jwt if account is logged in and request is to the api url
-        const account = this.accountService.accountValue;
-        const isLoggedIn = account && account.jwtToken;
-        const isApiUrl = request.url.startsWith(environment.apiUrl);
-        if (isLoggedIn && isApiUrl) {
+    intercept(request: HttpRequest<any>, next: HttpHandler) {
+        const user = this.accountService.accountValue;
+        if (user && user.jwtToken) {
             request = request.clone({
-                setHeaders: { Authorization: `Bearer ${account.jwtToken}` }
+                setHeaders: { Authorization: `Bearer ${user.jwtToken}` }
             });
         }
         return next.handle(request);
